fb307002fe680438fedddb9c81e934c7739d05a7
Ondrej Chaloupka <ochaloup@redhat.com>
2012-08-28 13:16:07 +0200

    Initial commit

---
+++ src/main/java/ochaloup/ClientReload.java
@@ -0,0 +1,33 @@
+package ochaloup;
+
+import java.net.InetAddress;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClientReload 
+{
+    public static void main( String[] args ) throws Exception {
+    	ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9999);
+    	
+    	ModelNode op = new ModelNode();
+    	op.get("operation").set("reload");
+    	op.get("admin-only").set(true);
+    	ModelNode status = client.execute(op);
+    	System.out.println("We are getting: " + status);
+    	
+        Thread.sleep(6000);
+
+        // client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9999);
+        ModelNode rop = new ModelNode();
+        rop.get("operation").set("read-attribute");
+        rop.get("name").set("server-state");
+        ModelNode status2 = client.execute(rop);
+        System.out.println("Server state: " + status2);
+        
+        client.close();
+    }
+}
